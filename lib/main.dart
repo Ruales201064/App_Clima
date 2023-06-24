17ffc2fa40d0fbad9497cb6489bb1a328d302d78
juanunity <83416779+Ruales201064@users.noreply.github.com>
2023-06-24 17:17:01 -0500

    secound commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  // widget raiz
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
