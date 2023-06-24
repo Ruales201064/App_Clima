f5260509a4545a7d2cc9349d39d3139266afc0e4
juanunity <83416779+Ruales201064@users.noreply.github.com>
2023-06-24 17:13:49 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/optic/app_weather/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.optic.app_weather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:app_weather/ui/home_page.dart';
+import 'package:flutter/material.dart';
+import '../ui/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Weather App',
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
